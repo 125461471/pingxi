4cabfb04655144f462bce87907239a421193646b
shenhl <shenhl@192.168.88.105>
2016-12-18 11:39:43 +0800

    添加redis缓存

--- src/main/java/com/heibaba/RedisConfig.java
+++ src/main/java/com/heibaba/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.heibaba;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,16 +15,66 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 
 @Configuration
 @EnableRedisHttpSession
 @EnableRedisRepositories
 public class RedisConfig {
-
+	
+	@Value("${spring.redis.host}")
+	private String host;
+	@Value("${spring.redis.port}")
+	private int port;
+	@Value("${spring.redis.database}")
+	private int database;
+	@Value("${spring.redis.password}") 
+	private String password;
+	@Value("${spring.redis.timeout}")
+	private int timeout;
+	@Value("${spring.redis.pool.max-active}")
+	private int  maxActive;
+	@Value("${spring.redis.pool.max-idle}")
+	private int  maxIdle;
+	@Value("${spring.redis.pool.max-wait}")
+	private long maxWait;
+	@Value("${spring.redis.pool.min-idle}")
+	private int minIdle;
+	
 	@Bean
-    public JedisConnectionFactory connectionFactory() {
-		return new JedisConnectionFactory(); 
-    }
+	public JedisPool redisPoolFactory() {
+		
+		JedisPoolConfig jedisPoolConfig = getJedisPoolConfig();
+		JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password, database);
+
+		return jedisPool;
+	}
+	
+    private JedisPoolConfig getJedisPoolConfig() {
+    	
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		jedisPoolConfig.setMaxTotal(maxActive);
+		jedisPoolConfig.setMaxIdle(maxIdle);
+		jedisPoolConfig.setMaxWaitMillis(maxWait);
+		jedisPoolConfig.setMinIdle(minIdle);
+		
+		return jedisPoolConfig;
+	}
+    
+	@Bean
+	public JedisConnectionFactory redisConnectionFactory() {
+		
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		factory.setHostName(host);
+		factory.setPort(port);
+		factory.setDatabase(database);
+		factory.setPassword(password);	
+		factory.setTimeout(timeout);
+		
+		return factory;
+	}
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
